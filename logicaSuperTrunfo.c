d538e5141c9b77d0f6e82cefc5015ad608d9abb4
Adson Daniel <adsondaniel09@gmail.com>
2025-03-22 00:06:15 +0000

    Eu Criei Logica SuperTrunfo

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,43 +1,104 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 2 - Comparação das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de comparação de cartas de cidades. 
-// Siga os comentários para implementar cada parte do desafio.
+int main(){
+  
+    // declaracao de variaveis
 
-int main() {
-    // Definição das variáveis para armazenar as propriedades das cidades
-    // Você pode utilizar o código do primeiro desafio
+    char estado1 = 'G', estado2 = 'B';
+    char codigo1[4] = "GO1", codigo2[4] = "BR2";
+    char nome1[50] = "Goiânia", nome2[50] = "Brasília";
+    int populacao1 = 1494599, populacao2 = 2817381;
+    float area1 = 729.29, area2 = 988.55;
+    float pib1 = 428.86, pib2 = 632.52;
+    int pontosturisticos1 = 50, pontosturisticos2 = 30;
 
+    // CARTA 1 //
+
+    printf("###\nCarta 1\n###");
+    printf("Codigo: %s\n", codigo1);
+    printf("Nome: %s\n", nome1);
+    printf("Populacao: %d\n", populacao1);
+    printf("Area: %f\n", area1);
+    printf("Pib: %f\n", pib1);
+    printf("Ponto Turisticos: %d\n", pontosturisticos1);
+
+    // CARTA 2 //
+
+    printf("###\nCarta 2\n###");
+    printf("Codigo: %s\n", codigo2);
+    printf("Nome: %s\n", nome2);
+    printf("Populacao: %d\n", populacao2);
+    printf("Area: %f\n", area2);
+    printf("Pib: %f\n", pib2);
+    printf("Ponto Turisticos: %d\n", pontosturisticos2);
+
+
+    //Cálculo de novos atributos
+    float densidade1 = populacao1/area1;
+    float densidade2 = populacao2/area2;
+    float pibpercapita1 = pib1/populacao1;
+    float pibpercapita2 = pib2/populacao2;
+
+    // Impressao dos novos atributos
+    printf("###\nNovos Atributos\n###");
+    printf("Densidade 1: %f\n", densidade1);
+    printf("Desindade 2: %f\n", densidade2);
+    printf("Pib per capita 1: %f\n", pibpercapita1);
+    printf("Pib per capita 2: %f\n", pibpercapita2);
+
+    // Menu de Comparacao
     
-    // Cadastro das Cartas:
-    // Implemente a lógica para solicitar ao usuário que insira os dados das cidades
-    // utilizando a função scanf para capturar as entradas.
-    // utilize o código do primeiro desafio
+    int opcao1, opcao2;
+    printf("\nEscolha dois atributos para comparacao:\n");
+    printf("1. População\n");
+    printf("2. Área\n");
+    printf("3. PIB\n");
+    printf("4. Pontos Turísticos\n");
+    printf("5. Densidade\n");
+    printf("6. PIB per capita\n");
 
-    // Exemplo:
-    // printf("Digite o código da cidade: ");
-    // scanf("%s", codigo);
-    // 
-    // (Repita para cada propriedade)
+    printf("Atributo 1: ");
+    scanf("%d", &opcao1);
+    printf("Atributo 2: ");
+    scanf("%d", &opcao2);
 
-    // Comparação de Cartas:
-    // Desenvolva a lógica de comparação entre duas cartas.
-    // Utilize estruturas de decisão como if, if-else para comparar atributos como população, área, PIB, etc.
+    // Logica da comparacao combinada
 
-    // Exemplo:
-    // if (populacaoA > populacaoB) {
-    //     printf("Cidade 1 tem maior população.\n");
-    // } else {
-    //     printf("Cidade 2 tem maior população.\n");
-    // }
+    printf("Resultado da comparacao\n");
 
-    // Exibição dos Resultados:
-    // Após realizar as comparações, exiba os resultados para o usuário.
-    // Certifique-se de que o sistema mostre claramente qual carta venceu e com base em qual atributo.
+    float valor1 = 0, valor2 = 0;
 
-    // Exemplo:
-    // printf("A cidade vencedora é: %s\n", cidadeVencedora);
+    switch (opcao1)
+    {
+    case 1: valor1 = populacao1; break;
+    case 2: valor1 = area1; break;
+    case 3: valor1 = pib1; break;
+    case 4: valor1 = pontosturisticos1; break;
+    case 5: valor1 = densidade1; break;
+    case 6: valor1 = pibpercapita1; break;
+    default: printf("Opcao invalida\n");
+    }
 
-    return 0;
-}
+    switch (opcao2)
+    { 
+    case 1: valor2 = populacao2; break;
+    case 2: valor2 = area2; break;
+    case 3: valor2 = pib2; break;
+    case 4: valor2 = pontosturisticos2; break;
+    case 5: valor2 = densidade2; break;
+    case 6: valor2 = pibpercapita2; break;
+    default: printf("Opcao invalida\n");
+    }
+
+     // Comparação dos atributos escolhidos
+     printf("Resultado da comparação:\n");
+     if (valor1 > valor2) {
+         printf("%s (%s) é maior que %s (%s) no atributo escolhido.\n", nome1, codigo1, nome2, codigo2);
+     } else if (valor1 < valor2) {
+         printf("%s (%s) é maior que %s (%s) no atributo escolhido.\n", nome2, codigo2, nome1, codigo1);
+     } else {
+         printf("Ambos têm o mesmo valor no atributo escolhido.\n");
+     }
+ 
+     return 0;
+ }
